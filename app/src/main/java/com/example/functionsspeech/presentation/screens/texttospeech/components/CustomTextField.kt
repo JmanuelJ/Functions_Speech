dd6ba72337c1b676a440c37eb90a86839ed20bef
JmanuelJ <penta21zona15@gmail.com>
2024-06-20 13:13:15 -0600

    remove warnings

--- app/src/main/java/com/example/functionsspeech/presentation/screens/texttospeech/components/CustomTextField.kt
+++ app/src/main/java/com/example/functionsspeech/presentation/screens/texttospeech/components/CustomTextField.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -16,14 +15,12 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.functionsspeech.R
 import com.example.functionsspeech.presentation.ui.theme.colorBlack
 import com.example.functionsspeech.presentation.ui.theme.colorRed
-import com.example.functionsspeech.presentation.ui.theme.colorWithe
 
 @Composable
 fun CustomTextField(
